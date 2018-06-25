1132107ffc1ea9ba86b9511cf1e9d736188ea1ba
AodaCat <pczqiang@gmail.com>
2018-06-25 23:29:11 +0800

    test3

--- first.java
+++ first.java
@@ -3,6 +3,6 @@
 public class first{
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println(new Date());
+//        System.out.println(new Date());
     }
 }
