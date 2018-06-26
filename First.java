3d3f8556d0426b09cfe72ccbe20e7ac1c40e2dad
AodaCat <pczqiang@gmail.com>
2018-06-26 15:30:30 +0800

    15

---
+++ First.java
@@ -0,0 +1,10 @@
+//First.java
+import java.util.*;
+
+public class First{
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println(new Date());
+        System.out.println("test");
+    }
+}
