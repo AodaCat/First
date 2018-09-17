7da0d0b57b36229ea050c9f9b2d1cb7bb32231be
AodaCat <pczqiang@gmail.com>
2018-09-17 20:32:13 +0800

    201809172029提交

--- First.java
+++ First.java
@@ -2,10 +2,53 @@
 import java.util.*;
 
 public class First{
+
+    // static int a = 8;
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println(new Date());
-        System.out.println("test");
-        System.out.println("git");
+
+        System.getProperties().list(System.out);
+        System.out.println(System.getProperty("user.name"));
+        System.out.println(System.getProperty("java.library.path"));
+
+        // First st1 = new First();
+        // System.out.println(st1.a);
+        // st1.a++;
+        // First st2 = new First();
+        // System.out.println(st1.a);
+        // System.out.println(st2.a);
+
+
+        // int a = 1;
+        // int b = a;
+        // a = 2;
+        // System.out.println("a=" + a);
+        // System.out.println("b=" + b);
+
+        // String aString = "astring";
+        // String bString = aString;
+        // aString = "bstring";
+        // System.out.println("aString="+aString);
+        // System.out.println("bString="+bString);
+        // System.out.println(aString.length());
+
+        // Character aCharacter = 'A';
+        // System.out.println(aCharacter.SIZE());
+
+
+        // System.out.println(1+2+"java"+3+4);
+        // System.out.println(new Date());
+        // System.out.println("test");
+        // System.out.println(4);
+    
+        // float c = 0.15f;
+        // System.out.println(c);
+        // double d = 3.14;
+        // System.out.println(d);
+        // String String = "String";
+        // System.out.println(String);
+
+        // System.out.println('"'+ "hello"+'"');
+        // System.out.println("\"hello\"");
     }
 }
