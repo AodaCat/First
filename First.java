cb771d65714cb6e4bb306bbf1b19874ccc43cef4
AodaCat <pczqiang@gmail.com>
2018-09-20 21:15:13 +0800

    201809202115

--- First.java
+++ First.java
@@ -4,12 +4,137 @@ import java.util.*;
 public class First{
 
     // static int a = 8;
+    
+    // int a;
+    // int b;
+    // int c;
+    // int d;
+    // int number;
+
+    // public void init(int number){
+    //     this.number = number;
+    //     this.a = number % 10;
+    //     this.b = number / 10 % 10;
+    //     this.c = number / 100 % 10;
+    //     this.d = number / 1000;
+    // }
+
 
     public static void main(String[] args) {
 
-        System.getProperties().list(System.out);
-        System.out.println(System.getProperty("user.name"));
-        System.out.println(System.getProperty("java.library.path"));
+        // for( int i=0; i<100; i++ ){
+        //     int sum = 0;
+        //     sum++;
+        //     System.out.print(sum);
+        // }
+
+        // int i = 3;
+        // if (i > 1){
+        //     int b = 1;
+        //     System.out.println(b);
+        // }
+        // if (i > 2){
+        //     int b = 2;
+        //     System.out.println(b);
+        // }
+        // if (i == 3){
+        //     int b = 3;
+        //     System.out.println(b);
+        // }
+
+        // Scanner input = new Scanner(System.in);
+        // int num = input.nextInt();
+        // int one = 0;
+        // int two = 0;
+        // int sum = 1;
+        // for (int i =0; i < num; i++){
+        //     System.out.print(sum + " ");
+        //     one = two;
+        //     two = sum;
+        //     sum = one + two;
+        // }
+
+        // First num = new First();
+        // for (int i = 1001; i < 10000; i++){
+        //     num.init(i);
+        //     if(i % 100 == 0){
+        //         continue;
+        //     }else if(num.number == (num.a * 10 + num.b) * (num.c * 10 + num.d)){
+        //         System.out.println(num.number + " = " + num.a + num.b + " * " + num.c + num.d);
+        //     }else if(num.number == (num.a * 10 + num.b) * (num.d * 10 + num.c)){
+        //         System.out.println(num.number + " = " + num.a + num.b + " * " + num.d + num.c);
+        //     }else if(num.number == (num.b * 10 + num.a) * (num.c * 10 + num.d)){
+        //         System.out.println(num.number + " = " + num.b + num.a + " * " + num.c + num.d);
+        //     }else if(num.number == (num.b * 10 + num.a) * (num.d * 10 + num.c)){
+        //         System.out.println(num.number + " = " + num.b + num.a + " * " + num.d + num.c);
+        //     }else if(num.number == (num.a * 10 + num.c) * (num.b * 10 + num.d)){
+        //         System.out.println(num.number + " = " + num.a + num.c + " * " + num.b + num.d);
+        //     }else if(num.number == (num.a * 10 + num.c) * (num.d * 10 + num.b)){
+        //         System.out.println(num.number + " = " + num.a + num.c + " * " + num.d + num.b);
+        //     }else if(num.number == (num.c * 10 + num.a) * (num.b * 10 + num.d)){
+        //         System.out.println(num.number + " = " + num.c + num.a + " * " + num.b + num.d);
+        //     }else if(num.number == (num.c * 10 + num.a) * (num.d * 10 + num.b)){
+        //         System.out.println(num.number + " = " + num.c + num.a + " * " + num.d + num.b);
+        //     }else if(num.number == (num.a * 10 + num.d) * (num.b * 10 + num.c)){
+        //         System.out.println(num.number + " = " + num.a + num.d + " * " + num.b + num.c);
+        //     }else if(num.number == (num.a * 10 + num.d) * (num.c * 10 + num.b)){
+        //         System.out.println(num.number + " = " + num.a + num.d + " * " + num.c + num.b);
+        //     }else if(num.number == (num.d * 10 + num.a) * (num.b * 10 + num.c)){
+        //         System.out.println(num.number + " = " + num.d + num.a + " * " + num.b + num.c);
+        //     }else if(num.number == (num.d * 10 + num.a) * (num.c * 10 + num.b)){
+        //         System.out.println(num.number + " = " + num.d + num.a + " * " + num.c + num.b);
+        //     }
+        // }
+
+        // while (condition()){
+        //     System.out.println("inside");
+        // }
+        // System.out.println("outside");
+
+
+        // Scanner inputScanner = new Scanner(System.in);
+        // char ziMu = inputScanner.next().charAt(0);
+        // if ((ziMu >='a' && ziMu <= 'z') || (ziMu >= 'A' && ziMu <= 'Z')){
+        //     System.out.println(true);
+        // }else{
+        //     System.out.println(false);
+        // }
+
+        // Scanner inputScanner = new Scanner(System.in);
+        // char number = inputScanner.next().charAt(0);
+        // if(number >='0' && number <= '9' ){
+        //     System.out.println(true);
+        // }else{
+        //     System.out.println(false);
+        // }
+        // System.out.println(number);
+        
+        // Scanner inputScanner = new Scanner(System.in);
+        // int number = inputScanner.nextInt();
+        // int a;
+        // int temp=number;
+        // int b;
+        // for(a=10; temp!=0; temp /= a){ 
+        //     b = temp % 10;
+        //     System.out.println(b);
+        // }
+        
+        // String s = "皮皮皮";
+        // System.out.println(s.getBytes(utf-8).length);
+        // System.out.println(s.getBytes(gbk).length);
+
+
+        // int a = 3;
+        // int b = 5;
+        // System.out.println(a = b);
+        
+        // System.out.println(3+-2);
+        // System.out.println(2++);//错误
+        // System.out.println(010);
+
+        // System.getProperties().list(System.out);
+        // System.out.println(System.getProperty("user.name"));
+        // System.out.println(System.getProperty("java.library.path"));
 
         // First st1 = new First();
         // System.out.println(st1.a);
@@ -51,4 +176,11 @@ public class First{
         // System.out.println('"'+ "hello"+'"');
         // System.out.println("\"hello\"");
     }
+
+    // public static boolean condition(){
+    //     boolean boo = Math.random() < 0.9;
+    //     System.out.print(boo + " ");
+    //     return boo;
+    // }
+
 }
